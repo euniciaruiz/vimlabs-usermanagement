b55383a00b27bc56e025fe55a2cc10b5c7647925
EuniceMaye Ruiz <eunice@g-angle.co.jp>
2017-03-31 15:59:25 +0800

    update save function

--- usermanagement/models.py
+++ usermanagement/models.py
@@ -1,6 +1,6 @@
 from __future__ import unicode_literals
 import datetime
-from django.db import models
+from django.db import models, IntegrityError
 from django.core.urlresolvers import reverse
 
 def _generateHash():
@@ -21,9 +21,24 @@ class User(models.Model):
         role = models.CharField(max_length=1, choices=ROLES, null=True)
         hash_code = models.CharField(max_length=500, primary_key=True, default=_generateHash, unique=True, editable=False)
 
-        def save(self):
-                self.hash_code = self.role+'-'+self.hash_code+self.user_name
-                super(User, self).save()
+        def save(self, *args, **kwargs):
+                if not self.hash_code:
+                        print 'no hash code'
+                        self.hash_code = self.role+'-'+_generateHash()+self.email
+                else:
+                        print 'has hash code'
+                success = False
+                while not success:
+                        try:
+                                super(User, self).save(*args, **kwargs)
+                                print self.hash_code
+                        except IntegrityError:
+                                raise
+                        else:
+                                success = True
+        
+                def __str__(self):
+                        return self.hash_code
         
         def __str__(self):
                 return self.hash_code
